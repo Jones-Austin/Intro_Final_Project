8560d84996bde9e0459418bfa5cb90604811eaa3
rmmilaz <114836862+rmmilaz@users.noreply.github.com>
2023-03-10 16:38:19 -0500

    Nav Bar JS
    Added:
    
    - Functions for the nav bar to just test te functionality of the buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("Test")
+
+
+
+// NAV BAR
+function homeButtonClick(){
+    alert("home button");
+  }
+
+function searchButtonClick(){
+    alert("search button");
+}
+
+function aboutButtonClick(){
+    alert("about us button");
+}
+
+function settingsButtonClick(){
+    alert("settings button");
+}
